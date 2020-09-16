32fdf525b8a8dba0e14b750f8791370afc979929
Everton Carneiro <everton.mercante@gmail.com>
2020-09-16 08:36:15 -0300

    Initial Commit

---
+++ MaintenanceReport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaintenanceReport
+//
+//  Created by Everton Carneiro on 16/09/20.
+//  Copyright © 2020 Everton Carneiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
